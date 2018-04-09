6e79f6bd732aa500935cb5c6f61ba33a24faca61
Griffone <thegriffones@gmail.com>
2018-04-09 16:16:34 +0200

    Added buttons to the popup window. Also made a helper class for popups to optimise for possible future popups. Made both buttons dismiss the popup window.

--- app/src/main/java/se/medituner/app/MojoScreen.java
+++ app/src/main/java/se/medituner/app/MojoScreen.java
@@ -11,10 +11,13 @@ import android.widget.PopupWindow;
 
 public class MojoScreen extends AppCompatActivity {
 
+    protected Popup questionPopup;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mojo_screen);
+        questionPopup = new Popup(this, R.layout.popup);
     }
 
     /**
@@ -24,31 +27,18 @@ public class MojoScreen extends AppCompatActivity {
      * @author Grigory Glukhov
      */
     public void onButtonShowPopupClick(View view) {
-
         // Get the reference to an existing layout.
-        ConstraintLayout mainLayout = findViewById(R.id.activity_mojo_screen);
+        View currentScreen = findViewById(R.id.activity_mojo_screen);
 
-        // Inflate the popup layout
-        LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
-        View popupView = inflater.inflate(R.layout.popup, null);
+        questionPopup.showPopupWindow(currentScreen);
+    }
 
-        // Create the popup window
-        int width = ConstraintLayout.LayoutParams.WRAP_CONTENT;
-        int height = ConstraintLayout.LayoutParams.WRAP_CONTENT;
-        boolean focusable = true;   // Tapping outside of the window will still be processed by it
+    public void onButtonYes(View view) {
+        // TODO: process taking medication
+        questionPopup.dismissPopupWindow();
+    }
 
-        final PopupWindow popupWindow = new PopupWindow(popupView, width, height, focusable);
-
-        // Show the popup window
-        popupWindow.showAtLocation(mainLayout, Gravity.CENTER, 0, 0);
-
-        // Dismiss the popup window when touched
-        popupView.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                popupWindow.dismiss();
-                return true;
-            }
-        });
+    public void onButtonNo(View view) {
+        questionPopup.dismissPopupWindow();
     }
 }
