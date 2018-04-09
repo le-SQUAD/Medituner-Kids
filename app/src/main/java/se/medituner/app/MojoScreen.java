e2c388953921676ff066ca46789b630c2a6314ae
Griffone <thegriffones@gmail.com>
2018-04-09 10:28:14 +0200

    inital commit
    Created the base app with 1 activity.

---
+++ app/src/main/java/se/medituner/app/MojoScreen.java
@@ -0,0 +1,13 @@
+package se.medituner.app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MojoScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mojo_screen);
+    }
+}
