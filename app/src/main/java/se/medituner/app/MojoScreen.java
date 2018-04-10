1033014d9a180b7d6f4a87b7de3ac51903a2bfa8
Relax2954 <sasa_lekic@live.com>
2018-04-10 16:31:50 +0200

    multiple activities

--- app/src/main/java/se/medituner/app/MojoScreen.java
+++ app/src/main/java/se/medituner/app/MojoScreen.java
@@ -8,27 +8,34 @@ import android.widget.Button;
 import android.widget.TextView;
 public class MojoScreen extends AppCompatActivity {
 
-    int streak = 0;
+    public static int streak=0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mojo_screen);
-        Button b= (Button) findViewById(R.id.button);
-        b.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MojoScreen.this, StreakPopUp.class));
-            }
-        });
+        TextView tv = findViewById(R.id.streak_text); //this is a textview for the current streak
+        tv.setText(Integer.toString(streak));
+//        Button b= (Button) findViewById(R.id.button);
+//        b.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                startActivity(new Intent(MojoScreen.this, StreakPopUp.class));
+//            }
+//        });
     }
 
     public void onButton(View view) {
-
-        TextView tv = findViewById(R.id.streak_text);
         streak++;
+        TextView tv = findViewById(R.id.streak_text); //this is a textview for the current streak
         tv.setText(Integer.toString(streak));
+        if (streak >= 3) {
+            startActivity(new Intent(MojoScreen.this, StreakPopUp.class));
+            tv.setText(Integer.toString(streak));
+
+        }
+
+
     }
-
-
 }
--- app/src/main/java/se/medituner/app/StreakPopUp.java
+++ app/src/main/java/se/medituner/app/StreakPopUp.java
@@ -1,20 +1,38 @@
 package se.medituner.app;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.util.DisplayMetrics;
 
-public class StreakPopUp extends Activity{
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class StreakPopUp extends Activity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.popwindowstreak);
-        DisplayMetrics dm =  new DisplayMetrics();
+        DisplayMetrics dm = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(dm);
-         int width= dm.widthPixels;
-         int height=dm.heightPixels;
-         getWindow().setLayout((int) (width*.4), (int) (height*.4));
+        int width = dm.widthPixels;
+        int height = dm.heightPixels;
+        getWindow().setLayout((int) (width * .4), (int) (height * .4));
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Do something after 5s = 5000ms
+                Intent intent=new Intent(StreakPopUp.this,MojoScreen.class);
+
+                startActivity(intent);
+            }
+        }, 2000);
     }
+
+
 }
+
