9003309c63a7dc85d971f2fb3d2c64550f659a70
Aleksandra <aleksandra.soltan@gmail.com>
2018-04-10 11:47:35 +0200

    Added mojo stand-in Animated mojo stand-in to jump

--- app/src/main/java/se/medituner/app/MojoScreen.java
+++ app/src/main/java/se/medituner/app/MojoScreen.java
@@ -1,5 +1,8 @@
 package se.medituner.app;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.TimeInterpolator;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,7 +10,15 @@ import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewPropertyAnimator;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.BounceInterpolator;
 import android.widget.PopupWindow;
+import android.widget.ImageView;
+import android.view.animation.TranslateAnimation;
+import android.view.animation.Interpolator;
 
 public class MojoScreen extends AppCompatActivity {
 
@@ -36,9 +47,24 @@ public class MojoScreen extends AppCompatActivity {
     public void onButtonYes(View view) {
         // TODO: process taking medication
         questionPopup.dismissPopupWindow();
+
+        final TimeInterpolator accelerateInterpolator = new AccelerateInterpolator();
+        final TimeInterpolator bounceInterpolator = new BounceInterpolator();
+        final ImageView image = (ImageView) findViewById(R.id.imageView2);
+        ViewPropertyAnimator viewPropertyAnimator = image.animate()
+                .translationY(-500).setInterpolator(accelerateInterpolator).setDuration(500).setListener(new AnimatorListenerAdapter() {
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        image.animate()
+                                .translationY(0)
+                                .setInterpolator(bounceInterpolator).setDuration(1000);
+                    }
+                });
+
     }
 
     public void onButtonNo(View view) {
         questionPopup.dismissPopupWindow();
     }
+
 }
