cb262d1eed1b33aef4874375328587bfba17c44e
Relax2954 <sasa_lekic@live.com>
2018-04-10 09:23:24 +0200

    actually included the changes

--- app/src/main/java/se/medituner/app/MojoScreen.java
+++ app/src/main/java/se/medituner/app/MojoScreen.java
@@ -1,7 +1,10 @@
 package se.medituner.app;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MojoScreen extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class MojoScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mojo_screen);
+        Button b= (Button) findViewById(R.id.button);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MojoScreen.this, StreakPopUp.class));
+            }
+        });
     }
 }
--- app/src/main/java/se/medituner/app/StreakPopUp.java
+++ app/src/main/java/se/medituner/app/StreakPopUp.java
@@ -1,4 +1,20 @@
 package se.medituner.app;
 
-public class StreakPopUp {
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.util.DisplayMetrics;
+
+public class StreakPopUp extends Activity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.popwindowstreak);
+        DisplayMetrics dm =  new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+         int width= dm.widthPixels;
+         int height=dm.heightPixels;
+         getWindow().setLayout((int) (width*.4), (int) (height*.4));
+    }
 }
