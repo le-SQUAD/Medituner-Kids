3a4c535b8af50ff17e84ace7b36733ce3fa58d48
Julia Danek <julia.danek123@gmail.com>
2018-04-10 10:39:38 +0200

    implemented streak counter

--- app/src/main/java/se/medituner/app/MojoScreen.java
+++ app/src/main/java/se/medituner/app/MojoScreen.java
@@ -2,12 +2,26 @@ package se.medituner.app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
 
 public class MojoScreen extends AppCompatActivity {
+
+    int streak = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mojo_screen);
     }
+
+    public void onButton(View view) {
+
+        TextView tv = findViewById(R.id.streak_text);
+        streak++;
+        tv.setText(Integer.toString(streak));
+    }
+
+
 }
