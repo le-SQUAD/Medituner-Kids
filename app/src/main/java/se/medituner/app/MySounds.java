6bf5755c4b56859dc2ec6384e245b8c9c874dfeb
Julia Danek <julia.danek123@gmail.com>
2018-04-11 09:56:31 +0200

    Design sounds

--- app/src/main/java/se/medituner/app/MySounds.java
+++ app/src/main/java/se/medituner/app/MySounds.java
@@ -4,6 +4,7 @@ import android.media.AudioAttributes;
 import android.media.AudioManager;
 import android.media.SoundPool;
 import android.os.Build;
+import android.os.Handler;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
@@ -19,8 +20,8 @@ import static se.medituner.app.R.id.button_jumpsound;
 
 public class MySounds extends AppCompatActivity {
 
-    public SoundPool soundPool;
-    public int jump, blink, happy;
+    private SoundPool soundPool;
+    private int jumping, blink, happy, cough, sad, star1, star2, star3;
 
 
     @Override
@@ -37,30 +38,83 @@ public class MySounds extends AppCompatActivity {
                         .build();
 
             soundPool = new SoundPool.Builder()
-                    // numebr of different sounds
-                    .setMaxStreams(3)
+                    // number of different sounds
+                    .setMaxStreams(8)
                     .setAudioAttributes(audioAttributes)
                     .build();
 
         } else {
             //first 3 = amout of different sounds
-            soundPool = new SoundPool(3, AudioManager.STREAM_MUSIC, 0);
+            soundPool = new SoundPool(8, AudioManager.STREAM_MUSIC, 0);
         }
 
-        jump = soundPool.load(this, R.raw.jump, 1);
+        jumping = soundPool.load(this, R.raw.jumping, 1);
         blink = soundPool.load(this, R.raw.blink, 1);
-        happy = soundPool.load(this, R.raw.blink, 1);
+        happy = soundPool.load(this, R.raw.happy, 1);
+        cough = soundPool.load(this, R.raw.cough, 1);
+        sad = soundPool.load(this, R.raw.sad, 1);
+        star1 = soundPool.load(this, R.raw.star1, 1);
+        star2 = soundPool.load(this, R.raw.star2, 1);
+        star3 = soundPool.load(this, R.raw.star3, 1);
 
     }
 
     public void playsound(View v) {
         switch (v.getId()){
-            case R.id.button_jumpsound:
-                soundPool.play(jump, 1, 1, 0, 0, 1);
+
+            case R.id.button_jumping:
+                final Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        soundPool.play(jumping, 1, 1, 0, 0, 1);
+                    }
+                }, 500);
+
+
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        soundPool.play(jumping, 1, 1, 0, 0, 1);
+                    }
+                }, 1000);
+
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        soundPool.play(jumping, 1, 1, 0, 0, 1);
+                    }
+                }, 1100);
+
+                break;
+
              case R.id.button_blink:
-                 soundPool.play(jump, 1, 1, 0, 0, 1);
+                 soundPool.play(blink, 1, 1, 0, 0, 1);
+                break;
+
             case R.id.button_happy:
-                soundPool.play(jump, 1, 1, 0, 0, 1);
+                soundPool.play(happy, 1, 1, 0, 0, 1);
+                break;
+
+            case R.id.button_cough:
+                soundPool.play(cough, 1, 1, 0, 0, 1);
+                break;
+
+            case R.id.button_sad:
+                soundPool.play(sad, 1, 1, 0, 0, 1);
+                break;
+
+            case R.id.button_star1:
+                soundPool.play(star1, 1, 1, 0, 0, 1);
+                break;
+
+            case R.id.button_star2:
+                soundPool.play(star2, 1, 1, 0, 0, 1);
+                break;
+
+            case R.id.button_star3:
+                soundPool.play(star3, 1, 1, 0, 0, 1);
+                break;
         }
 
     }
