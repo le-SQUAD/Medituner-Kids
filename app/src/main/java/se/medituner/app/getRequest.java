5ae056cc786303268f1d3a4aa0065a526e719ab2
Aleksandra <aleksandra.soltan@gmail.com>
2018-04-12 14:59:33 +0200

    Updated and tested get request with test server

--- app/src/main/java/se/medituner/app/getRequest.java
+++ app/src/main/java/se/medituner/app/getRequest.java
@@ -17,20 +17,23 @@ public class getRequest {
 
     public static void main(String[] args){
         try{
-            URL url = new URL("http://asthmatuner-dev-ayond.azurewebsites.net/api/ver1/treatmentplan");
+            URL url = new URL("http://now.httpbin.org");
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             connection.setRequestMethod("GET");
-            connection.setRequestProperty("Accept","api/ver1/treatmentplan");
+            connection.setRequestProperty("Accept","text/plain");
 
             if(connection.getResponseCode() != 200){
                 throw new RuntimeException("\t\n" +
                         "Errorcode=8000 , drug not found.\r\n" + connection.getResponseCode());
             }
-            BufferedReader br = new BufferedReader(new InputStreamReader(
-                    (connection.getInputStream())));
-            String tmp = "";
-            while((tmp = br.readLine()) != null ){
-                System.out.println(tmp);
+            BufferedReader reader = new BufferedReader(new
+                    InputStreamReader(connection.getInputStream()));
+
+            String line = reader.readLine();
+
+            while (line != null) {
+                System.out.println(line);
+                line = reader.readLine();
             }
 
             connection.disconnect();
