0184cd0b3f3e14cea76932f4ac053fc23185adf8
anorangesky <agnes.petajavaara@gmail.com>
2018-04-16 14:58:47 +0200

    Added interface and SystemClock

--- app/src/main/java/se/medituner/app/SchedulePopup.java
+++ app/src/main/java/se/medituner/app/SchedulePopup.java
@@ -1,13 +1,41 @@
 package se.medituner.app;
-import java.util.*;
-import java.time.*;
 
-public static final LocalTime MIN;
-public class SchedulePopup {
+import java.util.Calendar;
+
+//MIN HH:mm
+//public static final LocalTime MIN;
+
+public class SchedulePopup implements IClock{
 
 
-        static LocalTime time = new LocalTime.now();
-        if(time.equals(12:00:00:00){
-                questionPopup.showPopupWindow(currentScreen);
+
+        Calendar time;
+
+        public static boolean isItPopupTime(Calendar time) {
+
+            if (time.equals('09:00')){
+                return true;
+            }else if (time.equals('08:00')){
+                return true;
+            }else{
+                return false;
+            }
         }
+          /*  switch(time){
+                case 1:
+                    time.MIN.equals(09:00);
+                    questionPopup.showPopupWindow(currentScreen);
+                    break;
+                case 2:
+                    time.MIN.equals(10:00);
+                    questionPopup.showPopupWindow(currentScreen);
+                    break;
+            }
+            */
+        }
+
+
+
+
+
 }
