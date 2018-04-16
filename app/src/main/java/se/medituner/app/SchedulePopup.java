4c0889b6a1d16f901568972c35360744ce3795f9
vcvlk <vlk.vendela@gmail.com>
2018-04-16 14:00:45 +0200

    new class SchedulePopup

---
+++ app/src/main/java/se/medituner/app/SchedulePopup.java
@@ -0,0 +1,13 @@
+package se.medituner.app;
+import java.util.*;
+import java.time.*;
+
+public static final LocalTime MIN;
+public class SchedulePopup {
+
+
+        static LocalTime time = new LocalTime.now();
+        if(time.equals(12:00:00:00){
+                questionPopup.showPopupWindow(currentScreen);
+        }
+}
