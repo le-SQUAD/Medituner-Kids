698a365332d34a28abca0457e6f695f762c79cd8
anorangesky <agnes.petajavaara@gmail.com>
2018-04-24 15:55:41 +0200

    Increase score at time interval during game play

--- app/src/main/java/se/medituner/app/GameScreen.java
+++ app/src/main/java/se/medituner/app/GameScreen.java
@@ -1,11 +1,17 @@
 package se.medituner.app;
 
+import android.icu.text.UnicodeSetSpanner;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
-public class GameScreen extends AppCompatActivity {
+import java.util.Timer;
+import java.util.TimerTask;
 
+public class GameScreen extends AppCompatActivity {
+    TextView text;
+    int score;
+    Timer timer;
     GameSurfaceView glSurfaceView;
 
     @Override
@@ -14,13 +20,43 @@ public class GameScreen extends AppCompatActivity {
 
         glSurfaceView = new GameSurfaceView(this);
 
-       // setContentView(glSurfaceView);
         setContentView(R.layout.highscore_view);
 
-        //Set the "test"placeholder for high score in front
+        //Set the placeholder for high score in front
         glSurfaceView = findViewById(R.id.glSurfaceViewID);
-        TextView text = (TextView) findViewById(R.id.textView2);
+        text = findViewById(R.id.textView2);
         text.bringToFront();
 
+        /*
+        Adds to the score by one each 0.5 second
+        Updates directly in game view by layout
+        @Author Agnes Petäjävaara
+         */
+        timer = new Timer();
+        score = 0;
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                timerMethod();
+            }
+            }, 500, 500);
+        }
+
+        private void timerMethod(){
+        this.runOnUiThread(timer_tick);
+        }
+        private Runnable timer_tick = new Runnable() {
+            @Override
+            public void run() {
+                text.setText("score: " + score);
+                score++;
+                }
+        };
+
+    /*
+    Get the score to be able to save it to the high score list
+     */
+    public int getScore() {
+        return score;
     }
 }
