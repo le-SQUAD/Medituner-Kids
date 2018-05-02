2b611e2ab0781d6172782487575e3391ae488ac0
Julia Danek <julia.danek123@gmail.com>
2018-05-02 11:50:51 +0200

    Implementing backbutton in gamescreen

--- app/src/main/java/se/medituner/app/game/GameScreen.java
+++ app/src/main/java/se/medituner/app/game/GameScreen.java
@@ -2,8 +2,11 @@ package se.medituner.app.game;
 import se.medituner.app.*;
 
 import android.annotation.SuppressLint;
+import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.TextView;
 import java.io.IOException;
 import java.util.Timer;
@@ -41,7 +44,32 @@ public class GameScreen extends AppCompatActivity {
         scoreView.bringToFront();
 
         highScore.begin();
-    }
+
+        /*
+        If "BackButton" (backarrow) pressed - goes back to MojoScreen
+        @author Julia Danek
+        */
+
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        }
+
+        /*
+        If "BackButton" pressed - ends activity
+        @author Julia Danek
+        */
+
+        @Override
+        public boolean onOptionsItemSelected(MenuItem item){
+           int id = item.getItemId();
+
+           if (id == android.R.id.home){
+               //ends the activity
+               this.finish();
+           }
+        return super.onOptionsItemSelected(item);
+        }
 
     @Override
     protected void onStop() {
@@ -59,4 +87,6 @@ public class GameScreen extends AppCompatActivity {
             }
         });
     }
+
+
 }
